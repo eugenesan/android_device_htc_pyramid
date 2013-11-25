cc8fae2ce238117ce9016cd393a0509acda2b992
Sultanxda <sultanxda@gmail.com>
2013-11-25 08:24:53 -0800

    Add repo patcher
    This is designed to let us patch simple fixes to repos without having to fork and maintain them. Current patches include HTCZ715E's WiFi region code fix and a patch to make Torch stay on when the screen is off.
    
    The repo-resetter script should be run if you cannot repo sync due to unstaged changes. Run repo-patcher before every build with the command ./device/htc/pyramid/repo-patcher.sh
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

---
+++ repo-patcher.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo Patching each individual repo for regular building...
+cd frameworks/opt/telephony
+git reset --hard && git clean -f -d
+patch -p1 < ../../../device/htc/pyramid/patches/frameworks-opt-telephony.patch
+cd ../../../packages/apps/Torch
+git reset --hard && git clean -f -d
+patch -p1 < ../../../device/htc/pyramid/patches/packages-apps-torch.patch
+cd ../../..
