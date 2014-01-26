b0010cd033f7f3d664cdc55f427a92bb94a764d1
Sultanxda <sultanxda@gmail.com>
2014-01-25 21:59:32 -0800

    camerawrapper: Strip all options completely
    This is the pure wrapper base with no custom options. As-is, it does not affect the vendor module in any way. This is to start from scratch.

--- camerawrapper/CameraWrapper.cpp
+++ camerawrapper/CameraWrapper.cpp
@@ -22,7 +22,7 @@
 */
 
 //#define LOG_NDEBUG 0
-#define LOG_PARAMETERS
+//#define LOG_PARAMETERS
 
 #define LOG_TAG "CameraWrapper"
 #include <cutils/log.h>
@@ -33,8 +33,6 @@
 #include <hardware/camera.h>
 #include <camera/Camera.h>
 #include <camera/CameraParameters.h>
-
-const char KEY_VIDEO_HDR[] = "video-hdr";
 
 static android::Mutex gCameraWrapperLock;
 static camera_module_t *gVendorModule = 0;
@@ -94,138 +92,25 @@ static int check_vendor_module()
 
 static char * camera_fixup_getparams(int id, const char * settings)
 {
-    const char* recordingHint = "false";
-    const char* captureMode = "normal";
-    const char* rotation = "0";
-    const char* videoHDR = "false";
-
     android::CameraParameters params;
     params.unflatten(android::String8(settings));
-
-    if(params.get(android::CameraParameters::KEY_RECORDING_HINT))
-        recordingHint = params.get(android::CameraParameters::KEY_RECORDING_HINT);
-    if(params.get(android::CameraParameters::KEY_CAPTURE_MODE))
-        captureMode = params.get(android::CameraParameters::KEY_CAPTURE_MODE);
-    if(params.get(android::CameraParameters::KEY_ROTATION))
-        rotation = params.get(android::CameraParameters::KEY_ROTATION);
-    if(params.get(KEY_VIDEO_HDR))
-        videoHDR = params.get(KEY_VIDEO_HDR);
-
-    /* Face detection */
-    params.set(android::CameraParameters::KEY_MAX_NUM_DETECTED_FACES_HW, "0");
-    params.set(android::CameraParameters::KEY_MAX_NUM_DETECTED_FACES_SW, "0");
-
-    /* Hardware HDR */
-    if(strcmp(captureMode, "hdr") == 0) {
-        ALOGI("Scene-Mode: HDR.");
-        params.set(android::CameraParameters::KEY_SCENE_MODE, "hdr");
-    }
-
-    /* Video HDR */
-    if(strcmp(videoHDR, "true") == 0)
-        params.set(KEY_VIDEO_HDR, "on");
-    else if(strcmp(videoHDR, "false") == 0)
-        params.set(KEY_VIDEO_HDR, "off");
-
-    /* Back Camera */
-    if(id == 0) {
-        /* Photo Mode */
-        if(strcmp(recordingHint, "false") == 0) {
-            params.set(android::CameraParameters::KEY_SUPPORTED_SCENE_MODES, "off,auto,action,portrait,landscape,night,night-portrait,theatre,beach,snow,sunset,steadyphoto,fireworks,sports,party,candlelight,backlight,flowers,AR,text,hdr");
-        }
-        params.set(android::CameraParameters::KEY_FOCAL_LENGTH, "3.82");
-        params.set(android::CameraParameters::KEY_HORIZONTAL_VIEW_ANGLE, "69.6");
-        params.set(android::CameraParameters::KEY_VERTICAL_VIEW_ANGLE, "43.0");
-    }
-
-    /* Fix rotation missmatch */
-    if(strcmp(rotation, "90") == 0)
-        params.set(android::CameraParameters::KEY_ROTATION, "0");
-    else if(strcmp(rotation, "180") == 0)
-        params.set(android::CameraParameters::KEY_ROTATION, "90");
-    else if(strcmp(rotation, "270") == 0)
-        params.set(android::CameraParameters::KEY_ROTATION, "180");
 
     android::String8 strParams = params.flatten();
     char *ret = strdup(strParams.string());
 
-    ALOGI("%s: get parameters fixed up", __FUNCTION__);
+    ALOGD("%s: get parameters fixed up", __FUNCTION__);
     return ret;
 }
 
 static char * camera_fixup_setparams(int id, const char * settings)
 {
-    const char* previewSize = "0x0";
-    const char* recordingHint = "false";
-    const char* sceneMode = "auto";
-    const char* videoHDR = "false";
-
     android::CameraParameters params;
     params.unflatten(android::String8(settings));
-
-    // fix params here
-    if(params.get(android::CameraParameters::KEY_PREVIEW_SIZE))
-        previewSize = params.get(android::CameraParameters::KEY_PREVIEW_SIZE);
-    if(params.get(android::CameraParameters::KEY_RECORDING_HINT))
-        recordingHint = params.get(android::CameraParameters::KEY_RECORDING_HINT);
-    if(params.get(android::CameraParameters::KEY_SCENE_MODE))
-        sceneMode = params.get(android::CameraParameters::KEY_SCENE_MODE);
-    if(params.get(KEY_VIDEO_HDR))
-        videoHDR = params.get(KEY_VIDEO_HDR);
-
-    params.set(android::CameraParameters::KEY_GPU_EFFECT, "0_bypass"); // Bypass
-    params.set(android::CameraParameters::KEY_GPU_EFFECT_PARAM_0, "0,0,0,0");
-    params.set(android::CameraParameters::KEY_GPU_EFFECT_PARAM_1, "0,0,0,0");
-    params.set(android::CameraParameters::KEY_GPU_EFFECT_PARAM_2, "");
-    params.set(android::CameraParameters::KEY_GPU_EFFECT_PARAM_3, "0,0,0,0");
-
-    /* Face detection */
-    params.set(android::CameraParameters::KEY_MAX_NUM_DETECTED_FACES_HW, "0");
-    params.set(android::CameraParameters::KEY_MAX_NUM_DETECTED_FACES_SW, "0");
-
-    /* Photo Mode */
-    if(strcmp(recordingHint, "false") == 0) {
-        /* Back Camera */
-        if(id == 0) {
-            params.set(android::CameraParameters::KEY_CONTIBURST_TYPE, "unlimited");
-            params.set(android::CameraParameters::KEY_OIS_SUPPORT, "false");
-            params.set(android::CameraParameters::KEY_OIS_MODE, "off");
-
-            /* Hardware HDR */
-            if(strcmp(sceneMode, "hdr") == 0) {
-                ALOGI("Capture-Mode: HDR.");
-                params.set(android::CameraParameters::KEY_SCENE_MODE, "off");
-                params.set(android::CameraParameters::KEY_CAPTURE_MODE, "hdr");
-            } else {
-                ALOGI("Capture-Mode: Normal.");
-                params.set(android::CameraParameters::KEY_CAPTURE_MODE, "normal");
-
-                /* ZSL */
-                ALOGI("ZSL-Mode: enabled");
-                params.set(android::CameraParameters::KEY_ZSL, "on");
-                params.set(android::CameraParameters::KEY_CAMERA_MODE, "1");
-            }
-        }
-    /* Video Mode */
-    } else if(strcmp(recordingHint, "true") == 0) {
-        /* For 1080p videosnapshot feature */
-        if(strcmp(previewSize, "1920x1088") == 0)
-            params.set(android::CameraParameters::KEY_PICTURE_SIZE, "1920x1088");
-    }
-
-    /* Video HDR */
-    if(strcmp(videoHDR, "on") == 0) {
-        ALOGI("Video-HDR: enabled");
-        params.set(KEY_VIDEO_HDR, "true");
-    } else if(strcmp(videoHDR, "off") == 0) {
-        ALOGI("Video-HDR: disabled");
-        params.set(KEY_VIDEO_HDR, "false");
-    }
 
     android::String8 strParams = params.flatten();
     char *ret = strdup(strParams.string());
 
-    ALOGI("%s: set parameters fixed up", __FUNCTION__);
+    ALOGD("%s: set parameters fixed up", __FUNCTION__);
     return ret;
 }
 
